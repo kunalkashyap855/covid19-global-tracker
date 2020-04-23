495424a359f9b77c172419a77e5adf36b144133b
Kunal Kashyap <kunalkashyap855@gmail.com>
2020-04-24 04:16:54 +0530

    Updates

---
+++ precache-manifest.2bf45aedf85825c97d31c1a0f80dcf33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cab325ac31df8d0fafe10557b0828a4",
+    "url": "/covid19-global-tracker/index.html"
+  },
+  {
+    "revision": "465ce332c95baea102ad",
+    "url": "/covid19-global-tracker/static/css/main.fc869175.chunk.css"
+  },
+  {
+    "revision": "bf63c37f5b4d70eb6262",
+    "url": "/covid19-global-tracker/static/js/2.4d9d612f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-global-tracker/static/js/2.4d9d612f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465ce332c95baea102ad",
+    "url": "/covid19-global-tracker/static/js/main.927d5a03.chunk.js"
+  },
+  {
+    "revision": "118a72f574b39e13dfca",
+    "url": "/covid19-global-tracker/static/js/runtime-main.4f03e38c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-global-tracker/static/media/image.d7265326.png"
+  }
+]);
